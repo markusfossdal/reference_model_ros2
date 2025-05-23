91adb887cf2de215fbaf8607926730a24b417724
Markus Fossdal <51286688+markusfossdal@users.noreply.github.com>
2025-05-23 16:52:40 +0200

    minor changes

--- clean.sh
+++ clean.sh
@@ -1,1 +1,3 @@
+set -x          # shows each command and its arguments
 rm -rf build log install
+rm -rf compile_commands.json
--- compile_gcc.sh
+++ compile_gcc.sh
@@ -1,1 +1,3 @@
+set -x          # shows each command and its arguments
 colcon build --build-base build/release --cmake-args --preset release_gcc
+ln -sf build/compile_commands.json .
